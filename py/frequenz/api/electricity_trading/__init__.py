83996b71dcd8cd1d38e8203ac9c6c1fbb1dfb223
David Natingga <david.natingga@frequenz.com>
2023-12-07 08:33:55 +0100

    Initial commit
    Signed-off-by: David Natingga <david.natingga@frequenz.com>

---
+++ py/frequenz/api/electricity_trading/__init__.py
@@ -0,0 +1,7 @@
+# License: MIT
+# Copyright © 2023 Frequenz Energy-as-a-Service GmbH
+
+"""Specification for Electricity Trading API..
+
+TODO(cookiecutter): Add a more descriptive module description.
+"""
---
+++ pytests/test_electricity_trading.py
@@ -0,0 +1,25 @@
+# License: MIT
+# Copyright © 2023 Frequenz Energy-as-a-Service GmbH
+
+"""Tests for the frequenz.api.electricity_trading package."""
+
+
+def test_package_import() -> None:
+    """Test that the package can be imported."""
+    # pylint: disable=import-outside-toplevel
+    from frequenz.api import electricity_trading
+
+    assert electricity_trading is not None
+
+
+def test_module_import_components() -> None:
+    """Test that the modules can be imported."""
+    # pylint: disable=import-outside-toplevel
+    from frequenz.api.electricity_trading import electricity_trading_pb2
+
+    assert electricity_trading_pb2 is not None
+
+    # pylint: disable=import-outside-toplevel
+    from frequenz.api.electricity_trading import electricity_trading_pb2_grpc
+
+    assert electricity_trading_pb2_grpc is not None
